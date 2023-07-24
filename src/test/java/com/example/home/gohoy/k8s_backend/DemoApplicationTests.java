a520ae8f51a86c7821b7ed577ca129351eae0d55
gohoy <2738430398@qq.com>
2023-07-24 02:01:15 +0000

    one

---
+++ src/test/java/com/example/home/gohoy/k8s_backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home.gohoy.k8s_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
